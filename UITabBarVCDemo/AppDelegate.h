38e53ab2a91815cd3944934aa5c04b468fda5143
xulianpeng <xulianpengXLP@126.com>
2016-07-25 14:56:46 +0800

    Initial Commit

---
+++ UITabBarVCDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITabBarVCDemo
+//
+//  Created by xulianpeng on 16/7/25.
+//  Copyright © 2016年 xulianpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITabBarVCDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITabBarVCDemo
+//
+//  Created by xulianpeng on 16/7/25.
+//  Copyright © 2016年 xulianpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
